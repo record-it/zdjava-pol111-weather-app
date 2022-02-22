f517de38060b044e8e4557611c139747fa9cc954
cs <siwon.cezary@gmail.com>
2022-02-22 18:56:38 +0100

    repository

--- src/main/java/FxWeatherApp.java
+++ src/main/java/FxWeatherApp.java
@@ -2,12 +2,24 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import lombok.extern.java.Log;
+import repository.OpenWeatherRepository;
+import repository.WeatherRepository;
 
+@Log
 public class FxWeatherApp extends Application {
     private GridPane root = new GridPane();
     private Scene scene = new Scene(root, 400, 300);
+    private static String apiKey;
+    WeatherRepository weatherRepository = new OpenWeatherRepository(apiKey);
 
     public static void main(String[] args) {
+        if (args[0] == null){
+            System.out.println("Brak klucza!");
+            return;
+        }
+        apiKey = args[0];
+        log.info("Klucz: " + apiKey);
         launch(args);
     }
     @Override
--- src/main/java/repository/OpenWeatherRepository.java
+++ src/main/java/repository/OpenWeatherRepository.java
@@ -1,21 +1,32 @@
 package repository;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import model.CurrentWeather;
 import util.OpenWeatherURI;
 
 import java.io.IOException;
 import java.util.Optional;
 
+@Log
 public class OpenWeatherRepository implements WeatherRepository{
     private ApiRepository<CurrentWeather> weatherApiRepository = new ApiRepository<>(CurrentWeather.class);
-    private OpenWeatherURI uri = new OpenWeatherURI("asdasd");
+    private OpenWeatherURI uri;
+
+    public OpenWeatherRepository(String apiKey) {
+        uri = new OpenWeatherURI(apiKey);
+    }
+
     @Override
     public Optional<CurrentWeather> findCurrentWeather(String cityName) {
         try {
             return weatherApiRepository.getObject(uri.getURIByCity(cityName));
         } catch (IOException e) {
+            log.throwing(OpenWeatherRepository.class.getName(), "findCurrentWeather", e);
             return Optional.empty();
         } catch (InterruptedException e) {
+            log.throwing(OpenWeatherRepository.class.getName(), "findCurrentWeather", e);
             return Optional.empty();
         }
     }
